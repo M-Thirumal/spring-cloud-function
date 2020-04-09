3c9e43ffc2242d257e95b4a22bc416a1136c37cb
Thirumal <m.thirumal@hotmail.com>
2020-04-09 15:32:31 +0530

    Added Functions

--- src/main/java/in/thirumal/SpringCloudFunctionApplication.java
+++ src/main/java/in/thirumal/SpringCloudFunctionApplication.java
@@ -1,13 +1,36 @@
 package in.thirumal;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class SpringCloudFunctionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudFunctionApplication.class, args);
+	}
+	
+	@Bean
+	public Function<Flux<String>, Flux<String>> function() {
+		return input -> input.map(String::toUpperCase); 
+	}
+	
+	
+	@Bean
+	public Consumer<String> consume() {
+		return input -> System.out.println("In consume: " + input);
+	}
+	@Bean
+	public Supplier<Mono<String>> supply() {
+		return () -> Mono.just("hi from supply");
 	}
 
 }
